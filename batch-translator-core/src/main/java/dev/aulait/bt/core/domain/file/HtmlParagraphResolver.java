142c54cc6c082f0e1dee2de989109c1b1b60e5b3
motoki.himeno <motoki.himeno@monocrea.co.jp>
2024-09-19 15:05:17 +0900

    rm slf4j

--- batch-translator-core/src/main/java/dev/aulait/bt/core/domain/file/HtmlParagraphResolver.java
+++ batch-translator-core/src/main/java/dev/aulait/bt/core/domain/file/HtmlParagraphResolver.java
@@ -9,10 +9,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
-@Slf4j
 public class HtmlParagraphResolver implements ParagraphResolver {
 
   private static final List<String> IGNORE_TAGS =
